f3224fdae1711468815347f8a4bb2eb7e0192f9a
Menci <mencici@msn.com>
2026-07-30 14:21:04 +0800

    chore(web): order the model badge imports

--- apps/web/src/components/models/model-info-badges.tsx
+++ apps/web/src/components/models/model-info-badges.tsx
@@ -1,5 +1,5 @@
-import { Trans, useTranslation } from 'react-i18next';
 import type { ReactNode } from 'react';
+import { Trans, useTranslation } from 'react-i18next';
 
 import { effectiveUpstreams, modelBadges, type ModelBadge } from './model-badges';
 import type { ControlPlaneModel } from '../../api/types';
